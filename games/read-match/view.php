cf1c2acb480e25e4aa3d02e19e85b5efaf604954
Bobby Vaughn <rcvaughn2@gmail.com>
2025-12-05 08:07:21 -0500

    Delete games directory

--- games/read-match/view.php
+++
@@ -1,5 +0,0 @@
-<img id="word-image" src="" alt="Picture">
-
-<div id="sentence-list"></div>
-
-<button id="next-btn" class="next-btn hidden" onclick="loadLevel()">Next Picture ➡</button>
--- games/spell-it/view.php
+++
@@ -1,6 +0,0 @@
-<img id="word-image" src="" alt="Word">
-
-<div id="word-blanks" class="word-blanks"></div>
-<div id="letter-choices" class="letter-choices"></div>
-
-<button id="next-btn" class="next-btn hidden" onclick="loadLevel()">Next Word ➡</button>
