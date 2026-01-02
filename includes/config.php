9047dffecbd98bd49dd286fb7013dc49aa37bd44
Bobby Vaughn <rcvaughn2@gmail.com>
2026-01-02 15:31:04 -0500

    Update database configuration in config.php

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,10 @@
 <?php
 // includes/config.php
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'cms');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'db');
+define('DB_NAME', 'kidgames');
+define('DB_USER', 'kid_user');
+define('DB_PASS', 'kid_password');
 define('DB_CHARSET', 'utf8mb4');
 define('INVITE_CODE', 'FamilyGames');
 ?>
