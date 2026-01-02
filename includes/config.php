bcff6c0e7077b11152b56166f61d1b4d9e2b846f
Bobby Vaughn <rcvaughn2@gmail.com>
2026-01-01 20:16:08 -0500

    Update database configuration and invite code

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,10 @@
 <?php
 // includes/config.php
 
-define('DB_HOST', 'db5019126498.hosting-data.io');
-define('DB_NAME', 'dbs15032043');
-define('DB_USER', 'dbu875104');
-define('DB_PASS', 'Myahgrace5');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'cms');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 define('DB_CHARSET', 'utf8mb4');
-define('INVITE_CODE', 'FAMILYFUN');
+define('INVITE_CODE', 'FamilyGames');
 ?>
