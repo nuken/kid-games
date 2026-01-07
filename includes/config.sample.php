0bcf994a6417df684c54349536e993cc736c4d6e
Bobby Vaughn <rcvaughn2@gmail.com>
2026-01-07 07:49:07 -0500

    Add comments to database configuration constants
    Updated database configuration constants with comments for clarity.

--- includes/config.sample.php
+++ includes/config.sample.php
@@ -1,9 +1,9 @@
 <?php
 // includes/config.php
 
-define('DB_HOST', 'db');
-define('DB_NAME', 'kidgames');
-define('DB_USER', 'kid_user');
-define('DB_PASS', 'kid_password');
+define('DB_HOST', 'db'); //Change to actual DB host
+define('DB_NAME', 'kidgames'); //Change to actual DB Name
+define('DB_USER', 'kid_user'); //Change to actual DB User
+define('DB_PASS', 'kid_password'); //Change to actual DB Password
 define('DB_CHARSET', 'utf8mb4');
 ?>
