ba3b41f2497b7e584e52a620337a26fd0f46969e
Bobby Vaughn <rcvaughn2@gmail.com>
2026-01-07 08:07:51 -0500

    Remove comment about config.php file location
    Removed a comment about moving the config.php file.

--- includes/config.sample.php
+++ includes/config.sample.php
@@ -1,6 +1,6 @@
 <?php
 // includes/config.php
-
+//Recommended: Move your config.php file from the includes/ folder to the directory immediately above your main website folder. Modify path in db.php.
 define('DB_HOST', 'db'); //Change to actual DB host
 define('DB_NAME', 'kidgames'); //Change to actual DB Name
 define('DB_USER', 'kid_user'); //Change to actual DB User
