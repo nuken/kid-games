7428eafdeb1b775a509944a251387fc777b91fca
Bobby Vaughn <rcvaughn2@gmail.com>
2026-01-07 08:08:51 -0500

    Format DB configuration lines for clarity

--- includes/config.sample.php
+++ includes/config.sample.php
@@ -1,9 +1,9 @@
 <?php
 // includes/config.php
 // Recommended: Move your config.php file from the includes/ folder to the directory immediately above your main website folder. Modify path in db.php.
-define('DB_HOST', 'db'); //Change to actual DB host
-define('DB_NAME', 'kidgames'); //Change to actual DB Name
-define('DB_USER', 'kid_user'); //Change to actual DB User
-define('DB_PASS', 'kid_password'); //Change to actual DB Password
+define('DB_HOST', 'db'); // Change to actual DB host
+define('DB_NAME', 'kidgames'); // Change to actual DB Name
+define('DB_USER', 'kid_user'); // Change to actual DB User
+define('DB_PASS', 'kid_password'); // Change to actual DB Password
 define('DB_CHARSET', 'utf8mb4');
 ?>
